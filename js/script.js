24f75dd3f1971e924424a83c0c0ee04601dc1432
Rafael-Guerra-42 <guerra.rag.12@outlook.com>
2024-08-17 21:27:05 -0300

    closes #4 - Funcionalidade Cadastrar adicionada

---
+++ js/script.js
@@ -0,0 +1,29 @@
+indexIncrement = 1;
+function AddForm(){
+    indexIncrement++;
+    document.getElementById("dataInsertion").innerHTML+=
+    `
+    <div class="row">
+        <div class="col-1">${indexIncrement}</div>
+        <div class="col-3">${document.getElementById("name").value}</div>
+        <div class="col-3">${document.getElementById("email").value}</div>
+        <div class="col-3">${document.getElementById("phone").value}</div>
+        <div class="col-1">${document.getElementById("categorySelection").value}</div>
+        <div class="col-1">${SelectedRadioButton(document.getElementsByName("exampleRadios"))}</div>
+    </div>
+    <hr>
+    `;
+
+
+
+};
+function SelectedRadioButton(radioButton){
+    for (let i =0; i<radioButton.length; i++){
+
+        if (radioButton[i].checked){
+            return radioButton[i].value;
+        }
+
+    }
+};
+
