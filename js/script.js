f198efb4b00bfeae19385fc0da5196e438a9058d
Rafael-Guerra-42 <guerra.rag.12@outlook.com>
2024-08-17 22:06:48 -0300

    closes #5 - Validação adicionada (nao permitir cadastro com campos vazios)

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
 indexIncrement = 1;
+document.getElementById("alert").hide();
 function AddForm(){
-    indexIncrement++;
-    document.getElementById("dataInsertion").innerHTML+=
-    `
-    <div class="row">
-        <div class="col-1">${indexIncrement}</div>
-        <div class="col-3">${document.getElementById("name").value}</div>
-        <div class="col-3">${document.getElementById("email").value}</div>
-        <div class="col-3">${document.getElementById("phone").value}</div>
-        <div class="col-1">${document.getElementById("categorySelection").value}</div>
-        <div class="col-1">${SelectedRadioButton(document.getElementsByName("exampleRadios"))}</div>
-    </div>
-    <hr>
-    `;
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("phone").value = "";
+    document.getElementById("alertPlace").innerHTML=""
+    if(document.getElementById("name").value=="" || document.getElementById("email").value=="" || document.getElementById("phone").value==""){
+        document.getElementById("alertPlace").innerHTML+=
+        `
+        <div class="alert alert-warning" id="alert">
+            <strong>Favor preencher todos os campos para cadastrar.</strong>
+        </div>
+        `
+
+    }else
+    {
+        indexIncrement++;
+        document.getElementById("dataInsertion").innerHTML+=
+        `
+        <div class="row">
+            <div class="col-1">${indexIncrement}</div>
+            <div class="col-3">${document.getElementById("name").value}</div>
+            <div class="col-3">${document.getElementById("email").value}</div>
+            <div class="col-3">${document.getElementById("phone").value}</div>
+            <div class="col-1">${document.getElementById("categorySelection").value}</div>
+            <div class="col-1">${SelectedRadioButton(document.getElementsByName("exampleRadios"))}</div>
+        </div>
+        <hr>
+        `;
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+    
+    }
 };
 function SelectedRadioButton(radioButton){
     for (let i =0; i<radioButton.length; i++){
